436573bb1946f5867daad3c7c3f65ec978b7413a
Michael Zhang <35476432+M-ichae-l@users.noreply.github.com>
2023-09-21 19:45:43 +0800

    Update for unzip macos toolchain

---
+++ toolchains/ameba_d_toolchain_macos-1.0.1/lib/gcc/arm-none-eabi/6.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Users/jenkins/sdk-6/gcc-6/configure --prefix=/Users/jenkins/sdk-6/tmp/asdk-6.5.0-3409/release/asdk-6.5.0/darwin/newlib --build=x86_64-apple-darwin --host=x86_64-apple-darwin --target=arm-none-eabi --enable-languages=c,c++ --enable-threads --disable-shared --enable-lto --enable-multilib --enable-target-optspace --enable-__cxa_atexit --disable-sjlj-exceptions --disable-decimal-float --disable-tls --disable-nls --enable-libssp --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libffi --disable-libitm --disable-libsanitizer --with-arch=armv5te --with-gnu-ld --with-gnu-as --with-dwarf2 --with-headers=yes --with-newlib --with-sdk=asdk --with-sysroot='/Users/jenkins/sdk-6/tmp/asdk-6.5.0-3409/release/asdk-6.5.0/darwin/newlib' --with-build-sysroot=/Users/jenkins/sdk-6/tmp/asdk-6.5.0-3409/release/asdk-6.5.0/darwin/newlib --with-pkgversion='Realtek ASDK-6.5.0 Build 3409' --with-mpfr=/Users/jenkins/sdk-6/tmp/asdk-6.5.0-3409/stage/asdk-6.5.0/newlib/mpfr-build/out --with-gmp=/Users/jenkins/sdk-6/tmp/asdk-6.5.0-3409/stage/asdk-6.5.0/newlib/gmp-build/out --with-mpc=/Users/jenkins/sdk-6/tmp/asdk-6.5.0-3409/stage/asdk-6.5.0/newlib/mpc-build/out --with-isl=/Users/jenkins/sdk-6/tmp/asdk-6.5.0-3409/stage/asdk-6.5.0/newlib/isl-build/out --with-abi=aapcs";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs" }, { "arch", "armv5te" } };
