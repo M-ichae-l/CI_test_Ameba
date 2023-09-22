cfe610b86c33f69bf9c806d0ab37aea674e584c5
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2023-09-22 18:23:46 +0800

    Update amebapro2_ci.sh

--- amebapro2_ci.sh
+++ amebapro2_ci.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#sed -i -e 's/\r$//' NAME-OF-FILE.sh
 
 set -e
 
@@ -7,20 +8,21 @@ _normpath() {
 }
 
 ROOT_PATH=$(_normpath "$(dirname "$0")/..")
-TOOLCHAIN_PATH=$ROOT_PATH/toolchain_path
+TOOLCHAIN_PATH_NAME=toolchain_path
+TOOLCHAIN_PATH=$ROOT_PATH/$TOOLCHAIN_PATH_NAME
 
 unzip_ubuntu(){
     rm -rf $TOOLCHAIN_PATH
     echo "unzip_ubuntu"
     mkdir $TOOLCHAIN_PATH
     cd $ROOT_PATH/Arduino_package/release/
-    tar -jxvf ameba_pro2_toolchain_linux-1.0.1-p1.tar.bz2 -C $TOOLCHAIN_PATH
-    tar -jxvf ameba_pro2_toolchain_linux-1.0.1-p2.tar.bz2 -C $TOOLCHAIN_PATH
+    tar -jxvf ameba_pro2_toolchain_linux-1.0.1-p1.tar.bz2 -C ../../$TOOLCHAIN_PATH
+    tar -jxvf ameba_pro2_toolchain_linux-1.0.1-p2.tar.bz2 -C ../../$TOOLCHAIN_PATH
+    cd ../../$TOOLCHAIN_PATH
+    cp -R 1.0.1-p2 1.0.1-p1
 }
 
 echo $OS
 echo $OS
-echo $OSTYPE
-echo $OSTYPE
 unzip_ubuntu
 echo "END"
